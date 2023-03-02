96b0008733772fa848da7f81b593441a2f09d7a1
Shubham Sharma <shubham78067@gmail.com>
2023-03-02 11:22:34 +0530

    ok

---
+++ zrunways/webapp/controller/LandingPage.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("zrunways.controller.LandingPage", {
+            onInit: function () {
+
+            },
+            onPresBtn:function(){
+                debugger;
+            }
+
+
+        });
+    });
---
+++ zrunways/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zrunways/test/unit/controller/LandingPage.controller"
+], function () {
+	"use strict";
+});
